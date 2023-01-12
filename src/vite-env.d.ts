272b566d9c6ba3804f59f45c8a7e6702d678181a
Jack Ta <jack.th.ta@gmail.com>
2023-01-12 10:54:07 -0800

    chore: add env var types for intellisense
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_BASE_URL: string;
+  readonly VITE_TMDB_BEARER_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
