4c5d1b5c5e7782a9850a3410c9c4bd1bd71abb2a
Jack Ta <jack.th.ta@gmail.com>
2023-01-13 16:25:45 -0800

    feat: add more SVG components
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ src/components/SVGs/ChevronRightSVG.tsx
@@ -0,0 +1,21 @@
+function ChevronRightSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+    >
+      <path
+        d="m9 6 6 6-6 6"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+export default ChevronRightSVG;
---
+++ src/components/SVGs/TrendingSVG.tsx
@@ -0,0 +1,22 @@
+function TrendingSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="currentColor"
+    >
+      <path
+        d="M16 20v-8m0 0 3 3m-3-3-3 3m-9-1 8-8 3 3 5-5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default TrendingSVG;
