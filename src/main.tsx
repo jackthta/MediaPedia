62c585dbbf89838391f93d2446ef838d8c672315
Jack Ta <jack.th.ta@gmail.com>
2023-01-19 12:30:17 -0800

    feat: fetch TMDB API configuration on app initialization
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { fetchConfiguration } from "./redux/slices/tmdb-configuration";
 
 import router from "./router/router";
 import { RouterProvider } from "react-router-dom";
 
 import "./styles/index.scss";
+
+// Fetch TMDB system wide configuration information.
+// E.g., image backdrop width sizes
+// Source: https://developers.themoviedb.org/3/configuration/get-api-configuration
+store.dispatch(fetchConfiguration());
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import tvShowsReducer from "../slices/tv-show";
+import tmdbConfigurationReducer from "../slices/tmdb-configuration";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 const store = configureStore({
-  reducer: { tvShows: tvShowsReducer },
+  reducer: {
+    tvShows: tvShowsReducer,
+    tmdbConfiguration: tmdbConfigurationReducer,
+  },
 });
 
 export default store;
