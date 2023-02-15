be5c775c007564b4016edb48e251831d34dd2728
Jack Ta <jack.th.ta@gmail.com>
2023-02-15 12:42:35 -0800

    refactor: make `MovieSVG` inherit current color
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

--- src/components/SVGs/MovieSVG.tsx
+++ src/components/SVGs/MovieSVG.tsx
@@ -7,19 +7,19 @@ function MovieSVG() {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      color="#000"
+      color="currentColor"
       aria-hidden="true"
       focusable="false"
     >
       <path
         d="M7 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm10 0a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm-5-5a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-        stroke="#000"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M2 12c0 5.523 4.477 10 10 10s10-4.477 10-10S17.523 2 12 2 2 6.477 2 12zm0 0v10"
-        stroke="#000"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
