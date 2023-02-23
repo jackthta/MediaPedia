4de73ee168eeb54f2545369a8e5897838d3177d1
Jack Ta <jack.th.ta@gmail.com>
2023-02-23 11:19:30 -0800

    feat: add sun and moon SVG components
    Signed-off-by: Jack Ta <jack.th.ta@gmail.com>

---
+++ src/components/SVGs/MoonSVG.tsx
@@ -0,0 +1,20 @@
+function MoonSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992zm5 1a2 2 0 0 0 2 2 2 2 0 0 0-2 2 2 2 0 0 0-2-2 2 2 0 0 0 2-2m2 7h2m-1-1v2" />
+    </svg>
+  );
+}
+
+export default MoonSVG;
---
+++ src/components/SVGs/SunSVG.tsx
@@ -0,0 +1,21 @@
+function SunSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      width="24"
+      height="24"
+      color="currentColor"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        d="M12 .5v1.92m0 19.16v1.92M23.5 12h-1.92M2.42 12H.5M12 .5v1.92m0 19.16v1.92M23.5 12h-1.92M2.42 12H.5m19.63-8.13-1.35 1.35M5.22 18.78l-1.35 1.35m16.26 0-1.35-1.35M5.22 5.22 3.87 3.87m16.26 0-1.35 1.35M5.22 18.78l-1.35 1.35m16.26 0-1.35-1.35M5.22 5.22 3.87 3.87"
+      />
+      <circle fill="none" stroke="currentColor" cx="12" cy="12" r="6.71" />
+    </svg>
+  );
+}
+
+export default SunSVG;
